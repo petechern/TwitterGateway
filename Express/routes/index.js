d6a776e0f2fc8e9fbf388efa24132cac90b2db3c
Pete Chern <chernp@csu.fullerton.edu>
2015-03-23 20:54:31 -0700

    Express

---
+++ Express/routes/index.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+
+appdata = require('../data.json');
+
+router.get('/', function(req, res) {
+	res.render('index');
+});
+
+router.get('/propose', function(req, res) {
+	res.render('propose');
+});
+
+router.post('/propose', function(req, res) {
+	var proposal = req.body.proposal;
+	appdata["proposal"].push({"author": "Pete Chern", "content": proposal, "upvote": 0, "downvote": 0});
+
+	res.redirect('/vote');
+});
+
+router.get('/vote', function(req, res) {
+	var proposal = appdata.proposal;
+
+	res.render('vote.ejs', {"proposal": proposal} );
+});
+
+module.exports = router;
